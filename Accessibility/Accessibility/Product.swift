ecf6cc40687aa83becf9dfa3f45964381fe32881
Nicole Souvenir <nsouvenir@urbn.com>
2019-01-22 20:57:43 -0500

    added starting files to project

---
+++ Accessibility/Accessibility/Product.swift
@@ -0,0 +1,9 @@
+//
+//  Product.swift
+//  Accessibility
+//
+//  Created by Nicole Souvenir on 1/22/19.
+//  Copyright © 2019 Nicole Souvenir. All rights reserved.
+//
+
+import Foundation
---
+++ Accessibility/Accessibility/ViewControllers/CategoryVC.swift
@@ -0,0 +1,30 @@
+//
+//  CategoryVC.swift
+//  Accessibility
+//
+//  Created by Nicole Souvenir on 1/22/19.
+//  Copyright © 2019 Nicole Souvenir. All rights reserved.
+//
+
+import UIKit
+
+class CategoryVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
